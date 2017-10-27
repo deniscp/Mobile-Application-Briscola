a70ff40aeabcc56935aefa3ba1f01a190816695f
shafiei <rozashafiei.polimi@gmail.com>
2017-10-27 10:43:01 +0200

    Classes Robot and AI added to the project

---
+++ app/src/main/java/it/polimi/group06briscola/Model/AI.java
@@ -0,0 +1,18 @@
+package it.polimi.group06briscola.Model;
+
+
+/**
+ * The AI class holds the logic or strategy of the Robot, which means how robot select a card in turn.
+ * @author Roza.
+ */
+
+class AI {
+
+    /**
+     * @return a card which has placed in the first position or 0 position of robot's hand.
+     */
+    int cardPositionInHands()
+    {
+        return 0;
+    }
+}
