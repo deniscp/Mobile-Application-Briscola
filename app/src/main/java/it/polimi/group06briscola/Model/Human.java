0dccd97a08dbd26041c472c6e3b7291cbb9cf84c
shafiei <rozashafiei.polimi@gmail.com>
2017-10-27 12:22:38 +0200

    "Modification on Human class"

--- app/src/main/java/it/polimi/group06briscola/Model/Human.java
+++ app/src/main/java/it/polimi/group06briscola/Model/Human.java
@@ -10,7 +10,7 @@ public class Human extends Player {
     /**
      * Constructor of human class which calls the superclass constructor(Player) in order to initialize its objects .
      */
-    public Human(int id, String username, int points) {
-        super(id, username, points);
+    public Human(int id, String username, int scores) {
+        super(id, username, scores);
     }
 }
