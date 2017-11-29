0d4a84a2bf89b6eb53b0d57aee4db612e5ab616c
Timo Zandonella <zandonel@in.tum.de>
2017-11-29 19:26:23 +0100

    first playable version

--- app/src/main/java/it/polimi/group06/activities/GameActivity.java
+++ app/src/main/java/it/polimi/group06/activities/GameActivity.java
@@ -1,165 +1,121 @@
 package it.polimi.group06.activities;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import it.polimi.group06.domain.*;
 
 import it.polimi.group06.R;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
-public class GameActivity extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
+import static it.polimi.group06.domain.Constants.FIRSTPLAYER;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
+    Button cardzero_button;
+    Button cardone_button;
+    Button cardtwo_button;
+    TextView humancard;
+    TextView robotcard;
+    TextView onecard;
+    TextView twocard;
+    TextView threecard;
+    TextView remaining;
+    TextView winner;
+    int i;
+    int j;
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
+    Game game;
+    Player human;
+    Player robot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_game);
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        cardzero_button = findViewById(R.id.zero);
+        cardone_button = findViewById(R.id.one);
+        cardtwo_button = findViewById(R.id.two);
+        humancard = findViewById(R.id.humancard);
+        robotcard = findViewById(R.id.robotcard);
+        onecard = findViewById(R.id.onecard);
+        twocard = findViewById(R.id.twocard);
+        threecard = findViewById(R.id.threecard);
+        remaining = findViewById(R.id.remaining);
+        winner = findViewById(R.id.winner);
 
+        game = new Game();
+        human = game.getPlayers()[0];
+        robot = game.getPlayers()[1];
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
+        setText();
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        cardzero_button.setClickable(true);
+        cardone_button.setClickable(true);
+        cardtwo_button.setClickable(true);
+
+        cardzero_button.setOnClickListener(this);
+        cardone_button.setOnClickListener(this);
+        cardtwo_button.setOnClickListener(this);
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
+    public void onClick(View v) {
+        if (v.getId() == R.id.zero) {
+            i = 0;
+        }
+        if (v.getId() == R.id.one) {
+            i = 1;
+        }
+        if (v.getId() == R.id.two) {
+            i = 2;
+        }
+        humancard.setText(human.getHand().get(i).toString());
+        game.playerPlaysCard(0, i);
+        robotcard.setText(robot.getHand().get(0).toString());
+        game.playerPlaysCard(1, 0);
+        game.newRound();
+        setText();
+        j++;
+        if(j==20){
+            onecard.setText("");
+            if (game.returnWinner()==FIRSTPLAYER){
+                winner.setText("Player wins with " + String.valueOf(human.getPlayerPoints()));
+            }else if(game.returnWinner()==FIRSTPLAYER){
+                winner.setText("Robot wins with " + String.valueOf(robot.getPlayerPoints()));
+            }else{
+                winner.setText("Draw");
+            }
+            Toast.makeText(this, "Game over!", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
+
+    void setText() {
+        TextView[] humanhand = {onecard, twocard, threecard};
+
+        if (human.getHand().size() == 3) {
+            for (int i = 0; i < humanhand.length; i++) {
+                humanhand[i].setText(human.getHand().get(i).toString());
+            }
+        } else if (human.getHand().size() == 2) {
+            for (int i = 0; i < humanhand.length - 1; i++) {
+                cardtwo_button.setEnabled(false);
+                humanhand[i].setText(human.getHand().get(i).toString());
+                humanhand[2].setText("");
+            }
+        } else if (human.getHand().size() == 1) {
+            cardone_button.setEnabled(false);
+            humanhand[1].setText("");
+            humanhand[0].setText(human.getHand().get(0).toString());
         } else {
-            show();
+            cardzero_button.setEnabled(false);
         }
-    }
 
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in delay milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+        remaining.setText(String.valueOf(game.getTable().getDeck().remaining()));
     }
 }
