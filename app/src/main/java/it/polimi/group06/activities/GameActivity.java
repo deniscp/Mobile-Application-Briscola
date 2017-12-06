a045e1b6818f3e8e9568e0123e5a4b0184ba1a73
Timo Zandonella <zandonel@in.tum.de>
2017-12-06 15:42:26 +0100

    new playable version in fullscreen

--- app/src/main/java/it/polimi/group06/activities/GameActivity.java
+++ app/src/main/java/it/polimi/group06/activities/GameActivity.java
@@ -9,13 +9,23 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
 import it.polimi.group06.R;
+import it.polimi.group06.domain.Game;
+import it.polimi.group06.domain.Player;
+
+import static it.polimi.group06.domain.Constants.FIRSTPLAYER;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -86,6 +96,20 @@ public class GameActivity extends AppCompatActivity {
         }
     };
 
+    Button cardzero_button;
+    Button cardone_button;
+    Button cardtwo_button;
+    TextView humancard;
+    TextView robotcard;
+    TextView remaining;
+    TextView winner;
+    int i;
+    int j;
+
+    Game game;
+    Player human;
+    Player robot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -114,7 +138,85 @@ public class GameActivity extends AppCompatActivity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        cardzero_button = findViewById(R.id.zero);
+        cardone_button = findViewById(R.id.one);
+        cardtwo_button = findViewById(R.id.two);
+        humancard = findViewById(R.id.humancard);
+        robotcard = findViewById(R.id.robotcard);
+        remaining = findViewById(R.id.remaining);
+        winner = findViewById(R.id.winner);
+
+        game = new Game();
+        human = game.getPlayers()[0];
+        robot = game.getPlayers()[1];
+
+        setText();
+
+        cardzero_button.setClickable(true);
+        cardone_button.setClickable(true);
+        cardtwo_button.setClickable(true);
+
+        cardzero_button.setOnClickListener(this);
+        cardone_button.setOnClickListener(this);
+        cardtwo_button.setOnClickListener(this);
     }
+
+    void setText() {
+        Button[] humanhand = {cardzero_button, cardone_button, cardtwo_button};
+
+        if (human.getHand().size() == 3) {
+            for (int i = 0; i < humanhand.length; i++) {
+                humanhand[i].setText(human.getHand().get(i).toString());
+            }
+        } else if (human.getHand().size() == 2) {
+            for (int i = 0; i < humanhand.length - 1; i++) {
+                cardtwo_button.setEnabled(false);
+                humanhand[i].setText(human.getHand().get(i).toString());
+                humanhand[2].setText("");
+            }
+        } else if (human.getHand().size() == 1) {
+            cardone_button.setEnabled(false);
+            humanhand[1].setText("");
+            humanhand[0].setText(human.getHand().get(0).toString());
+        } else {
+            cardzero_button.setEnabled(false);
+        }
+
+        remaining.setText(String.valueOf(game.getTable().getDeck().remaining()));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.zero) {
+            i = 0;
+        }
+        if (v.getId() == R.id.one) {
+            i = 1;
+        }
+        if (v.getId() == R.id.two) {
+            i = 2;
+        }
+        humancard.setText(human.getHand().get(i).toString());
+        game.playerPlaysCard(0, i);
+        robotcard.setText(robot.getHand().get(0).toString());
+        game.playerPlaysCard(1, 0);
+        game.newRound();
+        setText();
+        j++;
+        if (j == 20) {
+            cardzero_button.setText("");
+            if (game.returnWinner() == FIRSTPLAYER) {
+                winner.setText("Player wins with " + String.valueOf(human.getPlayerPoints()));
+            } else if (game.returnWinner() == FIRSTPLAYER) {
+                winner.setText("Robot wins with " + String.valueOf(robot.getPlayerPoints()));
+            } else {
+                winner.setText("Draw");
+            }
+            Toast.makeText(this, "Game over!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
