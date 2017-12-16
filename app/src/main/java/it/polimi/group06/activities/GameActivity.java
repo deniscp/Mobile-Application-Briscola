5ade0c6ca22a270ee20c044a4bab42ff20391e88
Timo Zandonella <zandonel@in.tum.de>
2017-12-16 22:43:25 +0100

    now playable with card images instead of boring text buttons

--- app/src/main/java/it/polimi/group06/activities/GameActivity.java
+++ app/src/main/java/it/polimi/group06/activities/GameActivity.java
@@ -3,12 +3,14 @@ package it.polimi.group06.activities;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,17 +20,18 @@ import java.util.List;
 import it.polimi.group06.InputHandler;
 import it.polimi.group06.OutputHandler;
 import it.polimi.group06.R;
+import it.polimi.group06.domain.Card;
 import it.polimi.group06.domain.Game;
 import it.polimi.group06.domain.Player;
 
-import static it.polimi.group06.domain.Constants.DRAW;
 import static it.polimi.group06.domain.Constants.FIRSTPLAYER;
 import static it.polimi.group06.domain.Constants.SECONDPLAYER;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button cardzero_button, cardone_button, cardtwo_button, saveandquit;
-    TextView humancard, robotcard, remaining, winner, briscola;
+    Button saveandquit;
+    TextView humancard, robotcard, remaining, winner;
+    ImageView cardzero_image, cardone_image, cardtwo_image, briscola_image;
 
     int i, color, numberoftimesplayerwon, numberoftimesrobotwon, numberofdraws;
 
@@ -50,15 +53,18 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         setContentView(R.layout.activity_game);
 
-        cardzero_button = findViewById(R.id.zero);
-        cardone_button = findViewById(R.id.one);
-        cardtwo_button = findViewById(R.id.two);
         humancard = findViewById(R.id.humancard);
         robotcard = findViewById(R.id.robotcard);
+
         remaining = findViewById(R.id.remaining);
-        briscola = findViewById(R.id.briscola);
         winner = findViewById(R.id.winner);
         saveandquit = findViewById(R.id.savequit_button);
+
+        cardzero_image = findViewById(R.id.cardzeroimage);
+        cardone_image = findViewById(R.id.cardoneimage);
+        cardtwo_image = findViewById(R.id.cardtwoimage);
+
+        briscola_image = findViewById(R.id.briscolaimage);
 
         Bundle extras = getIntent().getExtras();
         String msg = extras.getString("keyMessage");
@@ -67,56 +73,149 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             String config = InputHandler.getStringfromFile("savedgame", getApplicationContext());
             game = Game.initializeFromConf(config);
         }
-        if (msg.equals("newgame")){
+        if (msg.equals("newgame")) {
             game = new Game();
         }
 
         human = game.getPlayers()[0];
         robot = game.getPlayers()[1];
 
-        setText();
         getStatisticsFile();
         setSettingsfromFile();
 
-        cardzero_button.setClickable(true);
-        cardone_button.setClickable(true);
-        cardtwo_button.setClickable(true);
-
-        cardzero_button.setOnClickListener(this);
-        cardone_button.setOnClickListener(this);
-        cardtwo_button.setOnClickListener(this);
         saveandquit.setOnClickListener(this);
+
+        cardzero_image.setOnClickListener(this);
+        cardone_image.setOnClickListener(this);
+        cardtwo_image.setOnClickListener(this);
+
+        briscola_image.setImageDrawable(getCardDrawable(game.getTable().getBriscola()));
+
+        setHandCardImages();
 
         tStart = System.currentTimeMillis();
     }
 
-    void setText() {
-        Button[] humanhand = {cardzero_button, cardone_button, cardtwo_button};
 
-        if (human.getHand().size() == 3) {
-            for (int i = 0; i < humanhand.length; i++) {
-                humanhand[i].setText(human.getHand().get(i).toString());
+    void setHandCardImages() {
+        ImageView[] humanhand = {cardzero_image, cardone_image, cardtwo_image};
+        int decksize = game.getTable().getDeck().remaining();
+        int handsize = human.getHand().size();
+        if (decksize == 0) {
+            briscola_image.setImageDrawable(null);
+        }
+        if (handsize == 3) {
+            for (int i = 0; i < 3; i++) {
+                Card humancard = human.getHand().get(i);
+                humanhand[i].setImageDrawable(getCardDrawable(humancard));
             }
-        } else if (human.getHand().size() == 2) {
-            for (int i = 0; i < humanhand.length - 1; i++) {
-                cardtwo_button.setEnabled(false);
-                humanhand[i].setText(human.getHand().get(i).toString());
-                humanhand[2].setText("-");
+        } else if (handsize == 2) {
+            humanhand[2].setImageDrawable(null);
+            for (int i = 0; i < 2; i++) {
+                Card humancard = human.getHand().get(i);
+                humanhand[i].setImageDrawable(getCardDrawable(humancard));
             }
-        } else if (human.getHand().size() == 1) {
-            humanhand[1].setEnabled(false);
-            humanhand[2].setEnabled(false);
-            humanhand[1].setText("-");
-            humanhand[2].setText("-");
-            humanhand[0].setText(human.getHand().get(0).toString());
+        } else if (handsize == 1) {
+            Card humancard = human.getHand().get(0);
+            for (int i = 0; i < 2; i++) {
+                humanhand[i].setImageDrawable(null);
+            }
+            humanhand[0].setImageDrawable(getCardDrawable(humancard));
         } else {
-            for(int i=0;i<3;i++){
-                humanhand[i].setEnabled(false);
-                humanhand[i].setText("-");
+            for (int i = 0; i < 3; i++) {
+                humanhand[i].setImageDrawable(null);
             }
         }
         remaining.setText(String.valueOf(game.remainingCards()));
-        briscola.setText(String.valueOf(game.getTable().getBriscola()));
+    }
+
+    Drawable getCardDrawable(Card cardatposition) {
+        switch (cardatposition.toString()) {
+            case ("1B"):
+                return getResources().getDrawable(R.drawable.bastoni1);
+            case ("2B"):
+                return getResources().getDrawable(R.drawable.bastoni2);
+            case ("3B"):
+                return getResources().getDrawable(R.drawable.bastoni3);
+            case ("4B"):
+                return getResources().getDrawable(R.drawable.bastoni4);
+            case ("5B"):
+                return getResources().getDrawable(R.drawable.bastoni5);
+            case ("6B"):
+                return getResources().getDrawable(R.drawable.bastoni6);
+            case ("7B"):
+                return getResources().getDrawable(R.drawable.bastoni7);
+            case ("JB"):
+                return getResources().getDrawable(R.drawable.bastoni8);
+            case ("HB"):
+                return getResources().getDrawable(R.drawable.bastoni9);
+            case ("KB"):
+                return getResources().getDrawable(R.drawable.bastoni10);
+
+            case ("1S"):
+                return getResources().getDrawable(R.drawable.spade1);
+            case ("2S"):
+                return getResources().getDrawable(R.drawable.spade2);
+            case ("3S"):
+                return getResources().getDrawable(R.drawable.spade3);
+            case ("4S"):
+                return getResources().getDrawable(R.drawable.spade4);
+            case ("5S"):
+                return getResources().getDrawable(R.drawable.spade5);
+            case ("6S"):
+                return getResources().getDrawable(R.drawable.spade6);
+            case ("7S"):
+                return getResources().getDrawable(R.drawable.spade7);
+            case ("JS"):
+                return getResources().getDrawable(R.drawable.spade8);
+            case ("HS"):
+                return getResources().getDrawable(R.drawable.spade9);
+            case ("KS"):
+                return getResources().getDrawable(R.drawable.spade10);
+
+            case ("1C"):
+                return getResources().getDrawable(R.drawable.coppe1);
+            case ("2C"):
+                return getResources().getDrawable(R.drawable.coppe2);
+            case ("3C"):
+                return getResources().getDrawable(R.drawable.coppe3);
+            case ("4C"):
+                return getResources().getDrawable(R.drawable.coppe4);
+            case ("5C"):
+                return getResources().getDrawable(R.drawable.coppe5);
+            case ("6C"):
+                return getResources().getDrawable(R.drawable.coppe6);
+            case ("7C"):
+                return getResources().getDrawable(R.drawable.coppe7);
+            case ("JC"):
+                return getResources().getDrawable(R.drawable.coppe8);
+            case ("HC"):
+                return getResources().getDrawable(R.drawable.coppe9);
+            case ("KC"):
+                return getResources().getDrawable(R.drawable.coppe10);
+
+            case ("1G"):
+                return getResources().getDrawable(R.drawable.denari1);
+            case ("2G"):
+                return getResources().getDrawable(R.drawable.denari2);
+            case ("3G"):
+                return getResources().getDrawable(R.drawable.denari3);
+            case ("4G"):
+                return getResources().getDrawable(R.drawable.denari4);
+            case ("5G"):
+                return getResources().getDrawable(R.drawable.denari5);
+            case ("6G"):
+                return getResources().getDrawable(R.drawable.denari6);
+            case ("7G"):
+                return getResources().getDrawable(R.drawable.denari7);
+            case ("JG"):
+                return getResources().getDrawable(R.drawable.denari8);
+            case ("HG"):
+                return getResources().getDrawable(R.drawable.denari9);
+            case ("KG"):
+                return getResources().getDrawable(R.drawable.denari10);
+        }
+        return null;
     }
 
     @Override
@@ -125,33 +224,32 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case (R.id.savequit_button):
                 saveGame();
-                System.out.println("xxxxxxx");
-                cardplayed=false;
+                cardplayed = false;
                 finish();
                 break;
-            case (R.id.zero):
+            case (R.id.cardzeroimage):
                 amountpositionwasplayed[0] += 1;
                 i = 0;
-                cardplayed=true;
+                cardplayed = true;
                 break;
-            case (R.id.one):
+            case (R.id.cardoneimage):
                 amountpositionwasplayed[1] += 1;
                 i = 1;
-                cardplayed=true;
+                cardplayed = true;
                 break;
-            case (R.id.two):
+            case (R.id.cardtwoimage):
                 amountpositionwasplayed[2] += 1;
                 i = 2;
-                cardplayed=true;
+                cardplayed = true;
                 break;
         }
-        if(cardplayed) {
+        if (cardplayed) {
             humancard.setText(human.getHand().get(i).toString());
             game.playerPlaysCard(0, i);
             robotcard.setText(robot.getHand().get(0).toString());
             game.playerPlaysCard(1, 0);
             game.newRound();
-            setText();
+            setHandCardImages();
             if (human.getHand().size() == 0 || robot.getHand().size() == 0) {
                 endofgame();
             }
@@ -165,7 +263,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     void updateStatisticsFile() {
         String towrite = String.valueOf(amountpositionwasplayed[0]) + "," + String.valueOf(amountpositionwasplayed[1]) + "," + String.valueOf(amountpositionwasplayed[2])
-                + "," + String.valueOf(elapsedSeconds) + "," + String.valueOf(numberoftimesplayerwon) + "," + String.valueOf(numberoftimesrobotwon) +","+
+                + "," + String.valueOf(elapsedSeconds) + "," + String.valueOf(numberoftimesplayerwon) + "," + String.valueOf(numberoftimesrobotwon) + "," +
                 String.valueOf(numberofdraws);
 
         OutputHandler.writefile(towrite, "statistics", getApplicationContext());
@@ -173,16 +271,15 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     public void endofgame() {
         String text;
-        cardzero_button.setText("-");
         if (game.returnWinner() == FIRSTPLAYER) {
             text = "Player wins with " + String.valueOf(human.getPlayerPoints()) + " Points";
-            numberoftimesplayerwon +=1;
+            numberoftimesplayerwon += 1;
         } else if (game.returnWinner() == SECONDPLAYER) {
             text = "Robot wins with " + String.valueOf(robot.getPlayerPoints()) + " Points";
-            numberoftimesrobotwon +=1;
+            numberoftimesrobotwon += 1;
         } else {
             text = "Draw";
-            numberofdraws +=1;
+            numberofdraws += 1;
         }
         Toast.makeText(this, "Game over!", Toast.LENGTH_SHORT).show();
 
@@ -200,8 +297,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                     public void onClick(DialogInterface dialog, int whichButton) {
                         Intent intent = getIntent();
                         String msg = intent.getExtras().getString("keyMessage");
-                        if(msg.equals("fromsaved")){
-                            msg="newgame";
+                        if (msg.equals("fromsaved")) {
+                            msg = "newgame";
                             intent.putExtra("keyMessage", msg);
                         }
                         finish();
